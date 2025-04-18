a432084740e5b6e779323ea0a82bd6b520d12934
David Dorchies <david.dorchies@inrae.fr>
2025-04-18 11:09:52 +0200

    fix: missing export of CARD_transpose_data
    When the function is located in a file with data in the name or something like, it is ignored by devtools::document()
    
    WTF R bug

--- R/CARD_process.R
+++ R/CARD_process.R
@@ -478,3 +478,32 @@ sourceProcess = function(path, default = NULL) {
     rm(list = ls(envir = CARD), envir = CARD)
     return(Process)
 }
+
+#' Transpose dataEX data.frame returned by CARD_extraction
+#'
+#'
+#' @param dataEX A [data.frame] returned by CARD_extraction with argument `simplify=TRUE`
+#'
+#' @returns The transposed data frame with the first column as 'name' and following columns with the ID of the time series.
+#'
+#' @export
+#'
+CARD_transpose_data <- function(dataEX) {
+    stopifnot(is.data.frame(dataEX))
+    # Step 1: Extract the 'name' value
+    id <- dataEX$name
+
+    # Step 2: Drop the 'name' column and transpose the rest
+    transposed <- as.data.frame(t(dataEX[, -1]))
+
+    # Step 3: Convert to data.frame and add row names as a column
+    transposed_df <- data.frame(
+        name = rownames(transposed),
+        value = transposed[[1]]
+    )
+    colnames(transposed_df)[2] <- id # Rename the second column with the ID value
+
+    # Reset rownames
+    rownames(transposed_df) <- NULL
+    return(transposed_df)
+}
